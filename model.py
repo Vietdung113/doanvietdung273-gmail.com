73c63a73846592a3ecbb1ba53bf71ca75144bc22
vietdung113 <doanvietdung273@gmail.com>
2020-04-13 20:55:57 +0700

    add model

--- model.py
+++ model.py
@@ -1,16 +1,16 @@
-import os
-
-import torch
 import torch.nn as nn
-import torchvision
 import torch.nn.functional as F
-from torch import optim
+import torch
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from tqdm import tqdm
 
 
 class MaskConv2d(nn.Conv2d):
-    def __init__(self, mask_type, *args, conditional_size=None, color_conditioning=False, **kwargs):
-        assert mask_type in ['A', 'B']
-        super(MaskConv2d, self).__init__(*args, **kwargs)
+    def __init__(self, mask_type, *args, conditional_size=None,
+                 color_conditioning=False, **kwargs):
+        assert mask_type == 'A' or mask_type == 'B'
+        super().__init__(*args, **kwargs)
         self.conditional_size = conditional_size
         self.color_conditioning = color_conditioning
         self.register_buffer('mask', torch.zeros_like(self.weight))
@@ -19,15 +19,17 @@ class MaskConv2d(nn.Conv2d):
             if len(self.conditional_size) == 1:
                 self.cond_op = nn.Linear(conditional_size[0], self.out_channels)
             else:
-                self.cond_op = nn.Conv2d(conditional_size[0], self.out_channels, kernel_size=3, padding=1)
+                self.cond_op = nn.Conv2d(conditional_size[0], self.out_channels,
+                                         kernel_size=3, padding=1)
 
-    def forward(self, x, cond=None):
-        b = x.shape[0]
-        out = nn.Conv2d(x, self.weight * self.mask, self.bias, self.stride, self.padding, self.dilation,
-                        self.groups)
+    def forward(self, input, cond=None):
+        batch_size = input.shape[0]
+        out = F.conv2d(input, self.weight * self.mask, self.bias, self.stride,
+                       self.padding, self.dilation, self.groups)
         if self.conditional_size:
             if len(self.conditional_size) == 1:
-                out = out + self.cond_op(cond).view(b, -1, 1, 1)
+                # Broadcast across height and width of image and add as conditional bias
+                out = out + self.cond_op(cond).view(batch_size, -1, 1, 1)
             else:
                 out = out + self.cond_op(cond)
         return out
@@ -44,7 +46,7 @@ class MaskConv2d(nn.Conv2d):
                 self.mask[one_third_out:2 * one_third_out, :2 * one_third_in, k // 2, k // 2] = 1
                 self.mask[2 * one_third_out:, :, k // 2, k // 2] = 1
             else:
-                self.mask[one_third_out:one_third_out * 2, :one_third_in, k // 2, k // 2] = 1
+                self.mask[one_third_out:2 * one_third_out, :one_third_in, k // 2, k // 2] = 1
                 self.mask[2 * one_third_out:, :2 * one_third_in, k // 2, k // 2] = 1
         else:
             if mask_type == 'B':
@@ -90,41 +92,49 @@ class LayerNorm(nn.LayerNorm):
 
 
 class PixelCNN(nn.Module):
-    def __init__(self, input_shape, n_colors, n_filters=64, kernel_size=7, n_layer=5, conditional_size=None,
-                 use_resblock=False, color_conditioning=False):
+    def __init__(self, input_shape, n_colors, n_filters=64,
+                 kernel_size=7, n_layers=5,
+                 conditional_size=None, use_resblock=False,
+                 color_conditioning=False):
         super().__init__()
-        assert n_layer >= 2
+        assert n_layers >= 2
         n_channels = input_shape[0]
-        kwargs = dict(conditional_size=conditional_size, color_conditioning=color_conditioning)
+
+        kwargs = dict(conditional_size=conditional_size,
+                      color_conditioning=color_conditioning)
         if use_resblock:
             block_init = lambda: ResBlock(n_filters, **kwargs)
         else:
-            block_init = lambda: MaskConv2d('B', n_filters, n_filters, kernel_size=kernel_size,
+            block_init = lambda: MaskConv2d('B', n_filters, n_filters,
+                                            kernel_size=kernel_size,
                                             padding=kernel_size // 2, **kwargs)
-        model = nn.ModuleList(
-            [MaskConv2d('A', n_channels, n_filters, kernel_size=kernel_size, padding=kernel_size // 2, **kwargs)])
-        for _ in range(n_layer):
+
+        model = nn.ModuleList([MaskConv2d('A', n_channels, n_filters,
+                                          kernel_size=kernel_size,
+                                          padding=kernel_size // 2, **kwargs)])
+        for _ in range(n_layers):
             if color_conditioning:
                 model.append(LayerNorm(color_conditioning, n_filters // 3))
             else:
                 model.append(LayerNorm(color_conditioning, n_filters))
-            model.append(nn.ReLU())
-            model.append(block_init())
-        model.append(nn.ReLU())
-        model.append(MaskConv2d('B', n_filters, n_filters, 1, **kwargs))
-        model.append(nn.ReLU())
-        model.append(MaskConv2d('B', n_filters, n_colors * n_channels, 1, **kwargs))
+            model.extend([nn.ReLU(), block_init()])
+        model.extend([nn.ReLU(), MaskConv2d('B', n_filters, n_filters, 1, **kwargs)])
+        model.extend([nn.ReLU(), MaskConv2d('B', n_filters, n_colors * n_channels, 1, **kwargs)])
+
         if conditional_size:
             if len(conditional_size) == 1:
-                self.cond_op = lambda x: x  # no processing if one hot
+                self.cond_op = lambda x: x  # No preprocessing conditional if one hot
             else:
-                # fro grayscale pixelcnn
-                self.cond_op = nn.Sequential(nn.Conv2d(1, 64, 3, padding=1),
-                                             nn.ReLU(),
-                                             nn.Conv2d(64, 64, 3, padding=1),
-                                             nn.ReLU(),
-                                             nn.Conv2d(64, 64, 3, padding=1),
-                                             nn.ReLU())
+                # For Grayscale PixelCNN (some preprocessing on the binary image)
+                self.cond_op = nn.Sequential(
+                    nn.Conv2d(1, 64, 3, padding=1),
+                    nn.ReLU(),
+                    nn.Conv2d(64, 64, 3, padding=1),
+                    nn.ReLU(),
+                    nn.Conv2d(64, 64, 3, padding=1),
+                    nn.ReLU()
+                )
+
         self.net = model
         self.input_shape = input_shape
         self.n_colors = n_colors
@@ -133,7 +143,7 @@ class PixelCNN(nn.Module):
         self.conditional_size = conditional_size
 
     def forward(self, x, cond=None):
-        b = x.shape[0]
+        batch_size = x.shape[0]
         out = (x.float() / (self.n_colors - 1) - 0.5) / 0.5
         if self.conditional_size:
             cond = self.cond_op(cond)
@@ -142,75 +152,78 @@ class PixelCNN(nn.Module):
                 out = layer(out, cond=cond)
             else:
                 out = layer(out)
+
         if self.color_conditioning:
-            return out.view(b, self.n_channels, self.n_colors, *self.input_shape[1:]).permute(0, 2, 1, 3, 4)
+            return out.view(batch_size, self.n_channels, self.n_colors,
+                            *self.input_shape[1:]).permute(0, 2, 1, 3, 4)
         else:
-            return out.view(b, self.n_colors, *self.input_shape)
+            return out.view(batch_size, self.n_colors, *self.input_shape)
 
     def loss(self, x, cond=None):
-        return F.cross_entropy(self.forward(x, cond), x.long())
+        return F.cross_entropy(self(x, cond=cond), x.long())
 
-
-def train(model, train_loader, optimizer, epoch, grad_clip=None):
-    model.train()
-
-    train_losses = []
-    for x, _ in train_loader:
-        # x = x.cuda().contiguous()
-        loss = model.loss(x)
-        optimizer.zero_grad()
-        loss.backward()
-        if grad_clip:
-            torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
-        optimizer.step()
-        train_losses.append(loss.item())
-    return train_losses
-
-
-def eval_loss(model, data_loader):
-    model.eval()
-    total_loss = 0
-    with torch.no_grad():
-        for x, _ in data_loader:
-            # x = x.cuda().contiguous()
-            loss = model.loss(x)
-            total_loss += loss * x.shape[0]
-        avg_loss = total_loss / len(data_loader.dataset)
-
-    return avg_loss.item()
-
-
-quiet = False
-
-
-def train_epochs(model, train_loader, test_loader, train_args):
-    epochs, lr = train_args['epochs'], train_args['lr']
-    grad_clip = train_args.get('grad_clip', None)
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-
-    train_losses = []
-    test_losses = [eval_loss(model, test_loader)]
-    for epoch in range(epochs):
-        model.train()
-        train_losses.extend(train(model, train_loader, optimizer, epoch, grad_clip))
-        test_loss = eval_loss(model, test_loader)
-        test_losses.append(test_loss)
-        if not quiet:
-            print(f'Epoch {epoch}, Test loss {test_loss:.4f}')
-
-    return train_losses, test_losses
+    def sample(self, n, cond=None):
+        samples = torch.zeros(n, *self.input_shape).cuda()
+        with torch.no_grad():
+            for r in range(self.input_shape[1]):
+                for c in range(self.input_shape[2]):
+                    for k in range(self.n_channels):
+                        logits = self(samples, cond=cond)[:, :, k, r, c]
+                        probs = F.softmax(logits, dim=1)
+                        samples[:, k, r, c] = torch.multinomial(probs, 1).squeeze(-1)
+        return samples.permute(0, 2, 3, 1).cpu().numpy()
 
 
 if __name__ == '__main__':
-    mnist_data = torchvision.datasets.MNIST(os.path.dirname(__file__), download=False,
-                                            transform=torchvision.transforms.Compose(
-                                                [torchvision.transforms.ToTensor()]))
-    train_loader = torch.utils.data.DataLoader(mnist_data,
-                                               batch_size=4,
-                                               shuffle=True,
-                                               num_workers=4)
-    model = PixelCNN((1, 28, 28), 2, n_layer=5)
-    for images, _ in train_loader:
-        x = model(images)
-        print(0)
-        break
+    # mnist_data = torchvision.datasets.MNIST(os.path.dirname(__file__), download=False,
+    #                                         transform=torchvision.transforms.Compose(
+    #                                             [torchvision.transforms.ToTensor()]))
+    # train_loader = torch.utils.data.DataLoader(mnist_data,
+    #                                            batch_size=4,
+    #                                            shuffle=True,
+    #                                            num_workers=4)
+    from torchsummary import summary
+
+    # model
+    model = PixelCNN((3, 32, 32), 4, n_layers=5, use_resblock=True, color_conditioning=True, n_filters=120)
+    summary(model, (3, 32, 32))
+    import torch
+    import torchvision
+    import torchvision.transforms as transforms
+
+    # Data loader
+    tranfomer = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=tranfomer)
+    train_loader = DataLoader(trainset, batch_size=16,
+                              shuffle=True, num_workers=4)
+    n_batch = len(train_loader)
+    # optimizer
+    optimizer = Adam(model.parameters(), lr=0.01)
+    max_loss = float('inf')
+    epochs = 10
+    model_path = '/home/dungdv/PycharmProjects/pixel_cnn_plus/weight/model.pth'
+    for epoch in range(epochs):
+        running_loss = 0
+        for i, (images, _) in tqdm(enumerate(train_loader)):
+            loss = model.loss(images)
+            running_loss += loss.item() * images.shape[0]
+            loss.backward()
+            optimizer.step()
+            if i % 10 == 0:
+                print('Epochs: {}\tloss: {}'.format(epoch + 1, running_loss/(i+1)))
+
+            if i == n_batch:
+                if running_loss < max_loss:
+                    max_loss = running_loss
+                    print('saving model at: {}'.format(model_path))
+                    torch.save(model, model_path)
+            # break
+        # break
+
+# summary(model, input_size=(3, 28, 28))
+# for images, _ in train_loader:
+#     x = model(images)
+#     print(0)
+#     break
